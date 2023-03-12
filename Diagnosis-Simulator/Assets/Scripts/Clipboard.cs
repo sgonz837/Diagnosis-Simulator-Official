5bef602e6975db44a9f2ee4caf7f6cde2eb66f31
Szizzle2 <gonzalezsayan9131@gmail.com>
2023-03-12 16:24:33 -0700

    Clipboard Feature
    hit start on controller to have it appear

---
+++ Diagnosis-Simulator/Assets/Scripts/Clipboard.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Clipboard : MonoBehaviour
+{
+    public Transform head;
+    public float spawnDistance = 2;
+    public GameObject menu;
+    public InputActionProperty showButton;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (showButton.action.WasPressedThisFrame())
+        {
+            menu.SetActive(!menu.activeSelf);
+            menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+        }
+        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
+        menu.transform.forward *= -1;
+    }
+}
