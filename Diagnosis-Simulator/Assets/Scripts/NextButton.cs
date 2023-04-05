b0becaf97d6ced2e3a8d0ddf50b3b34f27839319
Szizzle2 <gonzalezsayan9131@gmail.com>
2023-04-04 23:04:38 -0700

    Updated More Features

--- Diagnosis-Simulator/Assets/Scripts/NextButton.cs
+++ Diagnosis-Simulator/Assets/Scripts/NextButton.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class NextButton : MonoBehaviour
 {
+    public Transform head;
+    public float spawnDistance = 2;
     public GameObject canvasToOpen;
+    public GameObject canvasToClose;
 
     public void OnButtonClick()
     {
-        GameObject canvas = GameObject.Find(canvasToOpen.name);
-        canvas.SetActive(true);
-        gameObject.SetActive(false);
+        //GameObject canvas = GameObject.Find(canvasToOpen.name);
+        //canvas.SetActive(true);
+        canvasToOpen.SetActive(!canvasToOpen.activeSelf);
+        canvasToOpen.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+        canvasToOpen.transform.LookAt(new Vector3(head.position.x, canvasToOpen.transform.position.y, head.position.z));
+        canvasToOpen.transform.forward *= -1;
+
+
+        canvasToClose.SetActive(false);
     }
 }
